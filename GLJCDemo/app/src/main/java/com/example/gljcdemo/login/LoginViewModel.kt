769351e4f0e5891610ad6a5ef88731f1de4066b0
Jian-2021 <2306517305@qq.com>
2022-07-14 11:03:23 +0800

    添加变量

--- GLJCDemo/app/src/main/java/com/example/gljcdemo/login/LoginViewModel.kt
+++ GLJCDemo/app/src/main/java/com/example/gljcdemo/login/LoginViewModel.kt
@@ -1,4 +1,29 @@
 package com.example.gljcdemo.login
 
+import androidx.compose.runtime.mutableStateOf
+
 class LoginViewModel {
+
+    // 状态数据初始化，初始化为字符串
+    var dataStr = mutableStateOf("-")
+
+    // 状态更新方法，将新输入的内容赋值给 MutableState<T> 对象的 value 值
+    fun onInputChange(inputContent: String) {
+        dataStr.value = inputContent
+    }
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
 }
